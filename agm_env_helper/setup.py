b6da5e3289b8a8bde04979976d1570fa12e41f6d
Alexander Chentsov <alexgeniusman@gmail.com>
2022-04-09 12:52:23 +0300

    0.1.2 release added

--- agm_env_helper/setup.py
+++ agm_env_helper/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='agm_env_helper',
-      version='0.1.1',
+      version='0.1.2',
       description='Custom env helper',
       packages=['agm_env_helper'],
       author_email='alexgeniusman@gmail.com',
